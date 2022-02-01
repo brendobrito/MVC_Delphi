46998c6fa56bea91c1183323f7f8d942e64e2478
brendobrito@outlook.com.br <brendobrito@outlook.com.br>
2022-02-01 15:21:49 -0300

    Versão 1
    Versão 1 do projeto de demonstração

---
+++ tst.pas
@@ -0,0 +1,102 @@
+unit tst;
+
+interface
+
+uses
+  System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
+  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs, FMX.Objects,
+  FMX.Ani, FMX.Effects, FMX.Layouts, FMX.TabControl, FMX.Controls.Presentation,
+  FMX.StdCtrls;
+
+type
+  TForm1 = class(TForm)
+    Layout1: TLayout;
+    Circle1: TCircle;
+    Label1: TLabel;
+    Label2: TLabel;
+    Layout2: TLayout;
+    Line1: TLine;
+    Layout4: TLayout;
+    Text1: TText;
+    Text2: TText;
+    Text3: TText;
+    Layout11: TLayout;
+    Text9: TText;
+    Rectangle7: TRectangle;
+    Layout13: TLayout;
+    Text10: TText;
+    Rectangle9: TRectangle;
+    Layout14: TLayout;
+    Rectangle10: TRectangle;
+    Label4: TLabel;
+    Layout3: TLayout;
+    ScrollBox1: TScrollBox;
+    TabControl1: TTabControl;
+    TabItem1: TTabItem;
+    GridPanelLayout1: TGridPanelLayout;
+    Layout5: TLayout;
+    Line2: TLine;
+    Line3: TLine;
+    Rectangle1: TRectangle;
+    ShadowEffect1: TShadowEffect;
+    FloatAnimation1: TFloatAnimation;
+    FloatAnimation2: TFloatAnimation;
+    Lbl_Btn_Fornecedor: TText;
+    Img_Btn_Fornecedor: TImage;
+    Layout6: TLayout;
+    Line4: TLine;
+    Line5: TLine;
+    Rectangle2: TRectangle;
+    ShadowEffect2: TShadowEffect;
+    FloatAnimation3: TFloatAnimation;
+    FloatAnimation4: TFloatAnimation;
+    Text4: TText;
+    Image1: TImage;
+    Layout7: TLayout;
+    Line6: TLine;
+    Rectangle3: TRectangle;
+    ShadowEffect3: TShadowEffect;
+    FloatAnimation5: TFloatAnimation;
+    FloatAnimation6: TFloatAnimation;
+    Text5: TText;
+    Image2: TImage;
+    Layout8: TLayout;
+    Line8: TLine;
+    Rectangle4: TRectangle;
+    ShadowEffect4: TShadowEffect;
+    FloatAnimation7: TFloatAnimation;
+    FloatAnimation8: TFloatAnimation;
+    Text6: TText;
+    Image3: TImage;
+    Layout9: TLayout;
+    Line9: TLine;
+    Rectangle5: TRectangle;
+    ShadowEffect5: TShadowEffect;
+    FloatAnimation9: TFloatAnimation;
+    FloatAnimation10: TFloatAnimation;
+    Text7: TText;
+    Image4: TImage;
+    Layout10: TLayout;
+    Rectangle6: TRectangle;
+    ShadowEffect6: TShadowEffect;
+    FloatAnimation11: TFloatAnimation;
+    FloatAnimation12: TFloatAnimation;
+    Text8: TText;
+    Image5: TImage;
+    TabItem2: TTabItem;
+    TabItem3: TTabItem;
+    StyleBook1: TStyleBook;
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.fmx}
+
+end.
